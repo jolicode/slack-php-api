210f738b891efd9c3bf2b7f00897651f93bc19eb
Nemanja Ivanis <shilele90@gmail.com>
2022-03-04 16:27:33 +0100

    added model

---
+++ generated/Model/OpenidConnectUserInfoGetResponsedefault.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of JoliCode's Slack PHP API project.
+ *
+ * (c) JoliCode <coucou@jolicode.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace JoliCode\Slack\Api\Model;
+
+class OpenidConnectUserInfoGetResponsedefault
+{
+    /**
+     * Note: PHP callstack is only visible in dev/qa.
+     *
+     * @var string|null
+     */
+    protected $callstack;
+    /**
+     * @var string|null
+     */
+    protected $error;
+    /**
+     * @var bool|null
+     */
+    protected $ok;
+
+    /**
+     * Note: PHP callstack is only visible in dev/qa.
+     */
+    public function getCallstack(): ?string
+    {
+        return $this->callstack;
+    }
+
+    /**
+     * Note: PHP callstack is only visible in dev/qa.
+     */
+    public function setCallstack(?string $callstack): self
+    {
+        $this->callstack = $callstack;
+
+        return $this;
+    }
+
+    public function getError(): ?string
+    {
+        return $this->error;
+    }
+
+    public function setError(?string $error): self
+    {
+        $this->error = $error;
+
+        return $this;
+    }
+
+    public function getOk(): ?bool
+    {
+        return $this->ok;
+    }
+
+    public function setOk(?bool $ok): self
+    {
+        $this->ok = $ok;
+
+        return $this;
+    }
+}
