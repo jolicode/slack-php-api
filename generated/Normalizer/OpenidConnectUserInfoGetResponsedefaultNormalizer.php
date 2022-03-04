35955a9a39656897398da9fce07110bc3282b387
Nemanja Ivanis <shilele90@gmail.com>
2022-03-04 16:29:54 +0100

    added normalizer

---
+++ generated/Normalizer/OpenidConnectUserInfoGetResponsedefaultNormalizer.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of JoliCode's Slack PHP API project.
+ *
+ * (c) JoliCode <coucou@jolicode.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace JoliCode\Slack\Api\Normalizer;
+
+use Jane\Component\JsonSchemaRuntime\Reference;
+use JoliCode\Slack\Api\Runtime\Normalizer\CheckArray;
+use Symfony\Component\Serializer\Normalizer\DenormalizerAwareInterface;
+use Symfony\Component\Serializer\Normalizer\DenormalizerAwareTrait;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class OpenidConnectUserInfoGetResponsedefaultNormalizer implements DenormalizerInterface, NormalizerInterface, DenormalizerAwareInterface, NormalizerAwareInterface
+{
+    use CheckArray;
+    use DenormalizerAwareTrait;
+    use NormalizerAwareTrait;
+
+    /**
+     * @return bool
+     */
+    public function supportsDenormalization($data, $type, $format = null)
+    {
+        return 'JoliCode\\Slack\\Api\\Model\\OpenidConnectUserInfoGetResponsedefault' === $type;
+    }
+
+    public function supportsNormalization($data, $format = null)
+    {
+        return \is_object($data) && 'JoliCode\\Slack\\Api\\Model\\OpenidConnectUserInfoGetResponsedefault' === \get_class($data);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function denormalize($data, $class, $format = null, array $context = [])
+    {
+        if (isset($data['$ref'])) {
+            return new Reference($data['$ref'], $context['document-origin']);
+        }
+        if (isset($data['$recursiveRef'])) {
+            return new Reference($data['$recursiveRef'], $context['document-origin']);
+        }
+        $object = new \JoliCode\Slack\Api\Model\OpenidConnectUserInfoGetResponsedefault();
+        if (null === $data || false === \is_array($data)) {
+            return $object;
+        }
+        if (\array_key_exists('callstack', $data) && null !== $data['callstack']) {
+            $object->setCallstack($data['callstack']);
+        } elseif (\array_key_exists('callstack', $data) && null === $data['callstack']) {
+            $object->setCallstack(null);
+        }
+        if (\array_key_exists('error', $data) && null !== $data['error']) {
+            $object->setError($data['error']);
+        } elseif (\array_key_exists('error', $data) && null === $data['error']) {
+            $object->setError(null);
+        }
+        if (\array_key_exists('ok', $data) && null !== $data['ok']) {
+            $object->setOk($data['ok']);
+        } elseif (\array_key_exists('ok', $data) && null === $data['ok']) {
+            $object->setOk(null);
+        }
+
+        return $object;
+    }
+
+    /**
+     * @return array|string|int|float|bool|\ArrayObject|null
+     */
+    public function normalize($object, $format = null, array $context = [])
+    {
+        $data = [];
+        if (null !== $object->getCallstack()) {
+            $data['callstack'] = $object->getCallstack();
+        }
+        $data['error'] = $object->getError();
+        $data['ok'] = $object->getOk();
+
+        return $data;
+    }
+}
